dd7b4fec7532eeaefadbd2e1f821ae3602e07ec2
Yeboah Ebenezer <ebeny7@yahoo.co.uk>
2017-10-20 12:40:26 +0000

    Adding width and height to table

--- designs.js
+++ designs.js
@@ -4,7 +4,7 @@
 // When size is submitted by the user, call makeGrid()
 const width = document.getElementById("input_width").value;
 const height = document.getElementById("input_height").value;
-var color = document.getElementById("colorPicker").value;
+var color = document.getElementById("colorPicker");
 var pixel = document.getElementById("pixel_canvas");
 var picker = document.getElementById("sizePicker");
 
@@ -17,14 +17,17 @@ picker.addEventListener("submit", function(e) {
 
 
 function makeGrid() {
-	for (var i = 0; i<=10; i++) {
+	while (width > 1) {
+		pixel.deleteRow(width);
+	for (var i = 0; i<=width; i++) {
 		var row = pixel.insertRow(i);
-			for (var x = 0; x <=10; x++) {
+			for (var x = 0; x <=height; x++) {
 				var cell = row.insertCell(x);
 					cell.addEventListener("click", function() {
 						this.style.backgroundColor = "red";
 					})
 			}
+	}
 	}
 }
 
