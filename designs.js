3b97ba2c75931f515180eb1a38944d7aaa4c50d6
Yeboah Ebenezer <ebeny7@yahoo.co.uk>
2017-10-26 21:02:42 +0000

    JS content review

--- designs.js
+++ designs.js
@@ -1,12 +1,9 @@
 
 //submit function to click on Grid height and Grid width, and call makeGrid function//
 document.getElementById("sizePicker").addEventListener("submit", function (e) {
-	//console.log(e);
+	console.log(e);
 	e.preventDefault();
-	makeGrid();
-	while (row.length > width + 1 && cell.length > height + 1)	{
-		console.log(pixel.deleteRow(width));
-	}	
+	makeGrid();	
 })
 
 
@@ -16,13 +13,13 @@ function makeGrid() {
 	var width = document.getElementById("input_width").value;
 	var height = document.getElementById("input_height").value;
 	var color = document.getElementById("colorPicker").value;
-	for (var i=0; i<=width; i++) {
+	for (var i=0; i<=height; i++) {
 		row = pixel.insertRow(i);//creating empty tr elements
-		for (var x=0; x<=height; x++) {
+		for (var x=0; x<=width; x++) {
 			cell = row.insertCell(x);//inserting cells into td elements
 				cell.addEventListener("click", function() {
 					this.style.backgroundColor = color;
 				})
 			}
 		}
-	}
+	} 
