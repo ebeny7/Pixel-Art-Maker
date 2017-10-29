c5c77661001c1a80c4a60f3e01ab8a07e2266b77
Yeboah Ebenezer <ebeny7@yahoo.co.uk>
2017-10-29 11:45:03 +0000

    Changes to color and grid size

--- designs.js
+++ designs.js
@@ -10,17 +10,17 @@ document.getElementById("sizePicker").addEventListener("submit", function (e) {
 
 //makeGrid function to draw grid and add color to cells
 function makeGrid() {
-	var pixel = document.getElementById("pixel_canvas");
+	let pixel = document.getElementById("pixel_canvas");
 	var width = document.getElementById("input_width").value;
 	var height = document.getElementById("input_height").value;
-	var color = document.getElementById("colorPicker").value;
 	for (var i=0; i<=height; i++) {
-		row = pixel.insertRow(i);//creating empty tr elements
+		var row = pixel.insertRow(i);//creating empty tr elements
 		for (var x=0; x<=width; x++) {
-			cell = row.insertCell(x);//inserting cells into td elements
-				cell.addEventListener("click", function() {//function to color the grid
-					this.style.backgroundColor = color;
-				})
-			}
+			var cell = row.insertCell(x);//inserting cells into td elements
+			cell.addEventListener("click", function() {//function to color the grid
+				this.style.backgroundColor = document.getElementById("colorPicker").value;
+			})
 		}
-	} 
+	}
+} 
+
